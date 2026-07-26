c93dabe71d4f7cf919a61adbe5c47593c042932e
Ítalo Borges <italo.borges1211@gmail.com>
2026-07-26 12:11:41 -0300

    routes2

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { vercelPreset } from '@vercel/react-router/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  ssr: true,
+  presets: [vercelPreset()]
 })
